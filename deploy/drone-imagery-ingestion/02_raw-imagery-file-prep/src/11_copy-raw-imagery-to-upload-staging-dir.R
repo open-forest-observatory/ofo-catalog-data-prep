9b3ebcc3a6d18eea10dec19580f639976930c057
Derek <22568494+youngdjn@users.noreply.github.com>
2025-04-28 00:51:29 +0000

    Script to copy files to upload staging dir

--- deploy/drone-imagery-ingestion/02_raw-imagery-file-prep/src/11_copy-raw-imagery-to-upload-staging-dir.R
+++ deploy/drone-imagery-ingestion/02_raw-imagery-file-prep/src/11_copy-raw-imagery-to-upload-staging-dir.R
@@ -1,11 +1,11 @@
 # Purpose: Take the imagery and associated metadata products (image points and mission polygons) for
-# each mission and hardlink them all into a single file tree that is ready to be published
-# (transferred to cyverse).
+# each mission and hardlink them all into a single file tree (all under one mission folder) that is
+# ready to be uploaded to CyVerse.
 
 library(furrr)
 library(tidyverse)
 
-copy_raw_imagery_to_publishable_tree = function(mission_id_foc) {
+copy_raw_imagery_to_upload_staging_dir = function(mission_id_foc) {
   # For tracking down the mission ID(s) that produces warnings when this function is called inside a
   # map() function, you can include this line and see which mission ID warning was printed just
   # before the real warning. warning(paste("Parsing EXIF for mission ID:", mission_id_foc))
@@ -16,7 +16,7 @@ copy_raw_imagery_to_publishable_tree = function(mission_id_foc) {
   # Hardlink all files from the publishable mission footprints dir to the unified publishable tree
   # First make the directories
   infile = file.path(FULL_METADATA_PER_MISSION_PATH, paste0(mission_id_foc, "_mission-metadata.gpkg"))
-  outfile = file.path(PUBLISHABLE_DATA_TREE, mission_id_foc, "mission-metadata", paste0(mission_id_foc, "_mission-metadata.gpkg"))
+  outfile = file.path(UPLOAD_STAGING_DIR_PATH, mission_id_foc, "metadata-mission", paste0(mission_id_foc, "_mission-metadata.gpkg"))
   outdir = dirname(outfile)
   dir.create(outdir, recursive = TRUE)
 
@@ -28,7 +28,7 @@ copy_raw_imagery_to_publishable_tree = function(mission_id_foc) {
   # Hardlink all files from the publishable mission points dir to the unified publishable tree
   # First make the directories
   infile = file.path(PARSED_EXIF_FOR_RETAINED_IMAGES_PATH, paste0(mission_id_foc, "_image-metadata.gpkg"))
-  outfile = file.path(PUBLISHABLE_DATA_TREE, mission_id_foc, "image-metadata", paste0(mission_id_foc, "_image-metadata.gpkg"))
+  outfile = file.path(UPLOAD_STAGING_DIR_PATH, mission_id_foc, "metadata-images", paste0(mission_id_foc, "_image-metadata.gpkg"))
   outdir = dirname(outfile)
   dir.create(outdir, recursive = TRUE)
 
@@ -39,9 +39,9 @@ copy_raw_imagery_to_publishable_tree = function(mission_id_foc) {
 
   # Hardlink all files from the publishable images dir to the unified publishable tree
   # First make the directories
-  infiles = list.files(file.path(PUBLISHABLE_IMAGES_PATH, mission_id_foc), full.names = FALSE, recursive = TRUE)
-  infiles_full = file.path(PUBLISHABLE_IMAGES_PATH, mission_id_foc, infiles)
-  outfiles = file.path(PUBLISHABLE_DATA_TREE, mission_id_foc, infiles)
+  infiles = list.files(file.path(IMAGERY_ZIP_AND_EXAMPLES_PATH, mission_id_foc), full.names = FALSE, recursive = TRUE)
+  infiles_full = file.path(IMAGERY_ZIP_AND_EXAMPLES_PATH, mission_id_foc, infiles)
+  outfiles = file.path(UPLOAD_STAGING_DIR_PATH, mission_id_foc, infiles)
   outdirs = unique(dirname(outfiles))
 
   walk(outdirs, dir.create, recursive = TRUE)
