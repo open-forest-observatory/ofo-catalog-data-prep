36fc304f77bacc79eeccddbc48f651daa7a4b73c
Derek <22568494+youngdjn@users.noreply.github.com>
2025-04-28 19:01:43 +0000

    Run NRS first

--- deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_allprojects.sh
+++ deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_allprojects.sh
@@ -1,3 +1,12 @@
+echo "2024-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
+Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
+
+echo "2020-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
+Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
+
+echo "2023-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
+Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
+
 echo "2019-focal" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
 Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
 
@@ -5,9 +14,6 @@ echo "2020-dispersal" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/dron
 Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
 
 echo "2020-fuels" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
-Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
-
-echo "2020-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
 Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
 
 echo "2021-early-regen" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
@@ -25,11 +31,6 @@ Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imag
 echo "2023-tahoe-aspen" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
 Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
 
-echo "2023-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
-Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
-
 echo "2024-ofo2" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
 Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
 
-echo "2024-ucnrs" > /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/imagery_project_name.txt
-Rscript --vanilla /ofo-share/repos-derek/ofo-catalog-data-prep/deploy/drone-imagery-ingestion/01_raw-imagery-metadata-prep/control_raw-imagery-metadata_01-to-07_perproject.R
