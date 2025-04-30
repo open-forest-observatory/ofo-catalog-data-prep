562f0f3e461ae48073d81b0eb129780a0b95a386
Derek <22568494+youngdjn@users.noreply.github.com>
2025-04-30 05:51:12 +0000

    Complete single-mission imagery download

--- deploy/drone-imagery-ingestion/03_photogrammetry/16_download-unzip-images.R
+++ deploy/drone-imagery-ingestion/03_photogrammetry/16_download-unzip-images.R
@@ -1,18 +1,18 @@
 # Purpose: For the specified mission ID, download the images from CyVerse and unzip them into a
 # staging folder for photogrammetry processing
 
-mission_id_foc = "001420"
+source("deploy/drone-imagery-ingestion/00_set-constants.R")
+
+mission_id_foc = "000574"
 
 # Destination folder:
 zip_filepath = file.path(
-  DOWNLOADED_IMAGERY_ZIP_DIR,
+  PHOTOGRAMMETRY_DIR, DOWNLOADED_IMAGERY_ZIP_SUBDIR,
   paste0(mission_id_foc, "_images.zip")
 )
 
 # Make sure the destination exists
 dir.create(dirname(zip_filepath), recursive = TRUE, showWarnings = FALSE)
-
-# !!!! Make sure remote file exists using iquest
 
 
 # Option 1: Download over HTTP
@@ -32,7 +32,9 @@ download.file(
 )
 
 # # Option 2: Download with iRODS (2-3x faster, but still just a tiny fraction of the pipeline time,
-# and more robust because it's not restricted to 10 simultaneous connections)
+# and more robust because it's not restricted to 10 simultaneous connections). But it will fail if
+# there are more than 10 simultaneous requests to irods, which is the case e.g. at the very start of
+# running the parallelized pipeline. So we are not using it until that can be fixed.
 
 # irods_path = paste0(
 #   CYVERSE_MISSIONS_DIR, mission_id_foc,
@@ -48,23 +50,32 @@ download.file(
 if (file.exists(zip_filepath)) {
   size = file.info(zip_filepath)$size
 
-  if (size < 10000) {
+  if (size < 1000000) {
     warning("Downloaded zip file is too small (is it an error message?) for mission: ", mission_id_foc)
+    return(FALSE)
   }
 } else {
   warning("Zip download failed (no server response or no remote file) for mission: ", mission_id_foc)
+  return(FALSE)
 }
 
-# Unzip
+# Unzip it
 
 unzip_dir = file.path(
-  INPUT_IMAGES_DIR,
+  PHOTOGRAMMETRY_DIR, INPUT_IMAGES_SUBDIR,
   mission_id_foc
 )
 
-if(!dir.exists(unzip_dir)) {
+if (!dir.exists(unzip_dir)) {
   dir.create(unzip_dir, recursive = TRUE)
 }
 
-command = paste("unzip", shQuote(zip_filepath), "-d", shQuote(unzip_dir))
-system(command, ignore.stdout = TRUE)
+command = paste("unzip -o -q", shQuote(zip_filepath), "-d", shQuote(unzip_dir))
+unzip_result = system(command)
+
+if (unzip_result != 0) {
+  warning("Unzip failed for mission: ", mission_id_foc)
+  return(FALSE)
+}
+
+return(TRUE)
